c89a53ede00d057e8983e2af2987ac5f4cd95d8d
KyleH57 <kyle.hashiro@gmail.com>
2021-07-26 16:36:39 -0700

    Added RS422 support

--- src/ICMD.cpp
+++ src/ICMD.cpp
@@ -4,36 +4,110 @@
 #include <CRC32.h>
 #include <SPI.h>
 
-void mdSetupTTL(SPIClass *spi, int csGPIO)
+#include "ICMD.hpp"
+
+class ICMD
 {
-    //ICMD setup TTL
-    spiWr(spi, 0x00, 0x01, csGPIO); //x2 24 bit counters, CNTR1 will be in bytes 0..2
-                                    //                    CNTR0 will be in bytes 3..5
-    spiWr(spi, 0x01, 0x81, csGPIO); //TTL Inputs, SPI IFC Priority
-    spiWr(spi, 0x02, 0x00, csGPIO);
-    spiWr(spi, 0x03, 0x00, csGPIO);
-    spiWr(spi, 0x04, 0x04, csGPIO); //Disable BiSS CH0
-    spiWr(spi, 0x30, 0x07, csGPIO); //Reset all counters
-}
+public:
+    ICMD(SPIClass *spi, int csGPIO, int config)
+    {
+        //set Chip Select
+        CS = csGPIO;
 
-void mdSetup422(SPIClass *spi, int csGPIO)
-{
-    //ICMD RS422
-    spiWr(spi, 0x00, 0x00, csGPIO); //24 bit
-    spiWr(spi, 0x01, 0x01, csGPIO); //SPI IFC Priority
-    spiWr(spi, 0x02, 0x00, csGPIO);
-    spiWr(spi, 0x03, 0x00, csGPIO);
-    spiWr(spi, 0x04, 0x04, csGPIO); //Disable BiSS CH0
-    spiWr(spi, 0x30, 0x07, csGPIO); //Reset all counters
-}
+        //configure the IC using SPI writes
+        if (config)
+        {
+            mdSetup422(spi, CS);
+        }
+        else
+        {
+            mdSetupTTL(spi, CS);
+        }
 
-void spiWr(SPIClass *spi, int addr, int data, int csPin)
-{
-    digitalWrite(csPin, LOW);
+        //initialize the byte buffer
+        for (int i = 0; i < 50; i++)
+        {
+            dataBuffer[i] = 0x0;
+        }
+    }
+    uint32_t getCounts()
+    {
+        const int readBytes = 3;
+        spiRd(0x08, readBytes, CS);
 
-    spi->transfer(addr);
+        //set counts
+        uint32_t counts = (dataBuffer[0] << 16) | (dataBuffer[1] << 8) | (dataBuffer[2]);
+        //sign extension
+        counts <<= 8;
+        counts >> 8;
 
-    spi->transfer(data);
+        return counts;
+    }
+    // Read device ID to verify we can talk to decoder
+    int systemCheck()
+    {
+        //read 'M'
+        spiRd(0x78, 1, CS);
 
-    digitalWrite(csPin, HIGH);
-}
+        if (dataBuffer[0] == 'M')
+        {
+            return 0;
+        }
+        return 1;
+    }
+
+private:
+    int CS;
+    static const int RDBUFFER_SIZE = 16;
+    uint8_t dataBuffer[RDBUFFER_SIZE];
+
+    void mdSetupTTL(SPIClass *spi, int csGPIO)
+    {
+        //ICMD setup TTL
+        spiWr(spi, 0x00, 0x00, csGPIO); //single 24 bit, can use 0x02 for single 48 bit counter.
+        spiWr(spi, 0x01, 0x81, csGPIO); //TTL Inputs, SPI IFC Priority
+        spiWr(spi, 0x02, 0x00, csGPIO);
+        spiWr(spi, 0x03, 0x00, csGPIO);
+        spiWr(spi, 0x04, 0x04, csGPIO); //Disable BiSS CH0
+        spiWr(spi, 0x30, 0x07, csGPIO); //Reset all counters
+    }
+
+    void mdSetup422(SPIClass *spi, int csGPIO)
+    {
+        //See page 25, table 61 for register map of counters
+        //update getCounts() to read 6 bytes if changing to 48 bit counter
+        //ICMD RS422
+        spiWr(spi, 0x00, 0x00, csGPIO); //single 24 bit, can use 0x02 for single 48 bit counter.
+        spiWr(spi, 0x01, 0x01, csGPIO); //SPI IFC Priority
+        spiWr(spi, 0x02, 0x00, csGPIO);
+        spiWr(spi, 0x03, 0x00, csGPIO);
+        spiWr(spi, 0x04, 0x04, csGPIO); //Disable BiSS CH0
+        spiWr(spi, 0x30, 0x07, csGPIO); //Reset all counters
+    }
+
+    void spiRd(int addr, int numBytes, int csPin)
+    {
+
+        digitalWrite(csPin, LOW); //pull SS slow to prep other end for transfer
+
+        vspi->transfer(0x80 | addr);
+
+        for (int i = 0; i < numBytes; i++)
+        {
+            dataBuffer[i] = vspi->transfer(0x00);
+        }
+
+        digitalWrite(csPin, HIGH);
+    }
+
+    void spiWr(SPIClass *spi, int addr, int data, int csPin)
+    {
+        digitalWrite(csPin, LOW);
+
+        spi->transfer(addr);
+
+        spi->transfer(data);
+
+        digitalWrite(csPin, HIGH);
+    }
+};
--- src/ICMD.hpp
+++ src/ICMD.hpp
@@ -1,9 +1,29 @@
+#ifndef ICMD_H
+#define ICMD_H
+
 #include <Arduino.h>
 #include <CRC32.h>
 #include <SPI.h>
 
-void mdSetupTTL(SPIClass *spi, int csGPIO);
+class ICMD
+{
+public:
+    int gpioCS;
+    static const int RDBUFFER_SIZE;
+    ICMD(SPIClass *spi, int csGPIO, int config);
 
-void mdSetup422(SPIClass *spi, int csGPIO);
+    uint32_t getCounts();
 
-void spiWr(SPIClass *spi, int addr, int data, int csPin);
+    int systemCheck();
+
+private:
+    void mdSetupTTL(SPIClass *spi, int csGPIO);
+
+    void mdSetup422(SPIClass *spi, int csGPIO);
+
+    void spiRd(int addr, int numB, int csPin);
+
+    void spiWr(SPIClass *spi, int addr, int data, int csPin);
+};
+
+#endif
